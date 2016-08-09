9af725b0deadbce50cd04ed8c509ff096b88f131
Hannah Marlin <hmmarlin@gmail.com>
2016-08-08 23:05:51 -0700

    push latest js updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,75 @@
 // create an arary with sports
 
-
 var allSports = ["baseball", "basketball", "soccer", "football", "gymnastics", "boxing", "curling", "hockey", "wrestling", "dog surfing"]
 
-dispalyButtons();
+displayButtons();
 
-// $('.sport').on('click', displayGifs);
 $('#addSport').on('click', addSport);
-
 
 
 // create functionality to access Giphy API on click of button
 
 function displayGifs() {
-	   $('#display-gif').empty();
+    $('#display-gif').empty();
 
     var selection = $(this).data('name');
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + selection + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + selection + "&api_key=dc6zaTOxFJmzC&limit=5";
 
     $.ajax({ url: queryURL, method: 'GET' })
 
     .done(function(response) {
 
-            var results = response.data;
-            
-            for (var i = 0; i < results.length; i++) {
+        var results = response.data;
 
-                var rating = results[i].rating;
-                var sportsDiv = $('<div class="item">');
-                var p = $('<p>rating: ' + rating + '</p>');
+        console.log(results.length);
 
-                var sportsImage = $("<img>");
-                sportsImage.attr('src', results[i].images.fixed_height.url);
-                sportsImage.attr('alt', 'sports image');
-               
-                sportsDiv.append(p)
-                sportsDiv.append(sportsImage)
+        for (var i = 0; i < results.length; i++) {
 
-                $('#display-gif').prepend(sportsDiv);
-            }
-        });
+            var rating = results[i].rating;
+            var sportsDiv = $('<div class="item">');
+            var p = $('<p>rating: ' + rating + '</p>');
 
+            var sportsImage = $("<img>");
+            sportsImage.attr('src', results[i].images.fixed_height_still.url);
+            sportsImage.attr('data-animate', results[i].images.fixed_height.url)
+            sportsImage.attr('data-still', results[i].images.fixed_height_still.url)
+            sportsImage.attr('data-state', 'still')
+            sportsImage.attr('alt', 'sports image');
+            sportsImage.attr('class', 'pic-animate')
 
-
-
+            sportsDiv.append(p)
+            sportsDiv.append(sportsImage)
+            $('#display-gif').prepend(sportsDiv);
+        }
+    });
 };
+
+// functionality to animate the gifs on click
+
+function playGif() {
+    $('.pic-animate').on('click', function() {
+
+        var state = $(this).attr('data-state');
+
+        var animate = $(this).attr('data-animate');
+        var still = $(this).attr('data-still');
+
+        if (state === 'still') {
+            $(this).attr('src', animate)
+            $(this).attr('data-state', 'animate')
+            console.log('animate: ' + this.src);
+        } else {
+            $(this).attr('src', still)
+            $(this).attr('data-state', 'still')
+            console.log('still: ' + this.src);
+        }
+    });
+}
+
 
 // create function to make buttons show
 
-function dispalyButtons() {
+function displayButtons() {
 
     $('#buttons-area').empty();
 
@@ -64,17 +85,15 @@ function dispalyButtons() {
 
 // create functionality to add user input to array 
 
-function addSport () {
+function addSport() {
 
-		var newSport = $('#sport-input').val();
+    var newSport = $('#sport-input').val();
 
-		allSports.push(newSport);
-		
-		dispalyButtons();
+    allSports.push(newSport);
 
-		return false;
+    displayButtons();
+    return false;
 }
 
 $(document).on('click', '.sport', displayGifs);
-
-// create functionality to stop / start the GIF on click of button
+$(document).on('click', '.pic-animate', playGif);
